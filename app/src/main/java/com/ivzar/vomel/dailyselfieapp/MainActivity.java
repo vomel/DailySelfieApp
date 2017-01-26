f48fa61423157629384d685f3804ab41f798dfe5
vomel <vomel.ivzar@gmail.com>
2017-01-26 12:08:33 +0100

    Added menu

--- app/src/main/java/com/ivzar/vomel/dailyselfieapp/MainActivity.java
+++ app/src/main/java/com/ivzar/vomel/dailyselfieapp/MainActivity.java
@@ -1,14 +1,18 @@
 package com.ivzar.vomel.dailyselfieapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = "DailySelfie";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate: " + savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
@@ -17,6 +21,21 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+        Log.i(TAG, "onCreateOptionsMenu: " + menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(TAG, "onOptionsItemSelected: " + item + ":" + item.getItemId());
+        switch (item.getItemId()) {
+            case R.id.action_camera:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
