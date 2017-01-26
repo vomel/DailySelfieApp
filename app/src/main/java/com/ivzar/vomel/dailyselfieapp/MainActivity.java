6703a93ee85f91bba0475c8dcbb831efb846765c
vomel <vomel.ivzar@gmail.com>
2017-01-26 11:25:24 +0100

    Initial commit

---
+++ app/src/main/java/com/ivzar/vomel/dailyselfieapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ivzar.vomel.dailyselfieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+}
